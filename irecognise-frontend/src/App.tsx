1afb5b173b41c0d93db9de53e7bf6ce6d09b1b10
josephinehemingway <josephine.hemingway@gmail.com>
2022-09-09 17:56:09 +0800

    initialised home

--- irecognise-frontend/src/App.tsx
+++ irecognise-frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} >
+                    {/*<Route index element={<Home />} />*/}
+                </Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
---
+++ irecognise-frontend/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            HELLOOO IM HOME
+            Upload your photos here
+        </div>
+    );
+};
+
+export default Home;
