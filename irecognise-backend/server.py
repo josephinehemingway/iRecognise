973ee09cb6a9f5797b5c9f181833f9079a31896b
josephinehemingway <josephine.hemingway@gmail.com>
2022-12-15 12:48:20 +0800

    added add_to_blacklist endpoint

--- irecognise-backend/server.py
+++ irecognise-backend/server.py
@@ -1,8 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, Response
 from pymongo import MongoClient
 from datetime import datetime
 import json
-import jsonpickle
 from bson import json_util
 
 app = Flask(__name__)
@@ -15,36 +14,70 @@ blacklist_collection = db['blacklist']
 now = datetime.now()
 # dd/mm/YY H:M:S
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+#
+# post = {
+# 	"name": "Christian Hemingway",
+# 	"dob": '28/3/2000',
+# 	"status": 'wanted',
+# 	"gender": 'male',
+# 	"description": "Wanted for gaming too much",
+# 	"last_seen_location": None,
+# 	"last_seen_timestamp": None,
+# 	"last_modified": dt_string,
+# 	"created_at": dt_string,
+# }
 
-post = {
-	"_id": 2,
-	"name": "Christian Hemingway",
-	"dob": '28/3/2000',
-	"status": 'wanted',
-	"gender": 'male',
-	"description": "Wanted for gaming too much",
-	"last_seen_location": None,
-	"last_seen_timestamp": None,
-	"last_modified": dt_string,
-	"created_at": dt_string,
-}
-
-# @app.route('/blacklist', methods=["POST"])
-def add_to_blacklist(post):
-	blacklist_collection.insert_one(post)
-
-# API routes
 @app.route('/blacklist', methods=["GET"])
 def get_blacklist():
     results = list(blacklist_collection.find())
-    return jsonpickle.encode(results)
+    return Response(json.dumps(results,default=str),mimetype="application/json")
 
 @app.route('/suspect', methods=["GET"])
 def get_suspect():
-	_id = int(request.args.get('id'))
-	results = list(blacklist_collection.find({"_id": _id}))
+	suspectId = int(request.args.get('id'))
+	results = list(blacklist_collection.find({"suspectId": suspectId}))
 
 	return json.dumps(results[0], default=json_util.default)
 
+@app.route('/suspect', methods=["POST"])
+def add_to_blacklist():
+	post = request.json
+
+	new_post_object = {}
+	new_post_object['name'] = post['name']
+	new_post_object['age'] = post['age']
+	new_post_object['gender'] = post['gender']
+	new_post_object['status'] = post['status']
+	new_post_object['description'] = post['description']
+	new_post_object['last_seen_location'] = None
+	new_post_object['last_seen_timestamp'] = None
+	new_post_object['last_modified'] = dt_string
+	new_post_object['created_at'] = dt_string
+
+	new_post_result = blacklist_collection.insert_one(new_post_object)
+
+	message = {"msg": "Successfully added to blacklist!"}
+	resp = jsonify(message)
+	resp.status_code = 200
+
+	return resp
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Send message to the user with notFound 404 status."""
+    # Message to the user
+    message = {
+        "err":
+            {
+                "msg": "This route is currently not supported. Please refer API documentation."
+            }
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
 if __name__ == '__main__':
+# 	blacklist_collection.delete_many({})
+# 	print('deleted')
 	app.run(debug=True, threaded=True)
