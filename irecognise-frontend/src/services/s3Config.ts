b93be19fcc6fd1a79fc51d04dd960c06b6dade5e
josephinehemingway <josephine.hemingway@gmail.com>
2022-12-22 00:51:50 +0800

    updated env naming

--- irecognise-frontend/src/services/s3Config.ts
+++ irecognise-frontend/src/services/s3Config.ts
@@ -1,7 +1,7 @@
 export const s3Config = {
-    bucketName:  process.env.REACT_AWS_BUCKET_NAME!,
-    region: 'ap-southeast-1'!,
-    accessKeyId: process.env.ACCESS_KEY_ID!,
-    secretAccessKey: process.env.REACT_AWS_SECRET_ACCESS_KEY!,
+    bucketName:  process.env.REACT_APP_AWS_BUCKET_NAME!,
+    region: process.env.REACT_APP_AWS_REGION!,
+    accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID!,
+    secretAccessKey:  process.env.REACT_APP_AWS_SECRET_ACCESS_KEY!,
     // s3Url: 'https:/your-aws-s3-bucket-url/'     /* Optional */
 }
