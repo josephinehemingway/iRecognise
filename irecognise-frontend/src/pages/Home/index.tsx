72f467615c59e69c231ad7727e59ca0fba9560e8
josephinehemingway <josephine.hemingway@gmail.com>
2023-04-05 17:48:50 +0800

    removed default widgets and added instructions

--- irecognise-frontend/src/pages/Home/index.tsx
+++ irecognise-frontend/src/pages/Home/index.tsx
@@ -2,24 +2,23 @@ import React, {RefObject, useEffect, useRef, useState} from 'react';
 import './Home.css'
 import '../../components/Home/styles.css';
 import 'antd/dist/antd.min.css';
-import {StyledTitle, StyledSectionHeading, StyledLabel} from '../../components/reusable/styledText';
+import {StyledTitle, StyledSectionHeading, StyledLabel, StyledText} from '../../components/reusable/styledText';
 import {EditOutlined} from "@ant-design/icons";
 import {StyledButton} from "../../components/reusable/button";
 import Dashboard from "../../components/Home/Dashboard/Dashboard";
 import WidgetsDrawer from "../../components/Home/WidgetsDrawer/WidgetsDrawer";
 import {Layout} from "react-grid-layout";
-// import RecentActivitySection from "../../components/Home/RecentActivity";
 
 const Home: React.FC = () => {
     const [width, setWidth] = useState(0);
     const observedDiv: RefObject<HTMLDivElement> = useRef(null);
     const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
 
-    const defaultWidgets = [
-        { i: "QuickActions", x: 13, y: 0, w: 4, h: 2, minH: 2, minW: 4}
-    ];
+    // const defaultWidgets = [
+        // { i: "QuickActions", x: 13, y: 0, w: 4, h: 2, minH: 2, minW: 4}
+    // ];
 
-    const [widgets, setWidgets] = useState<Layout[]>(defaultWidgets);
+    const [widgets, setWidgets] = useState<Layout[]>([]);
 
     useEffect (() => {
         const storedWidgets = window.localStorage.getItem('WIDGET_LIST');
@@ -65,6 +64,18 @@ const Home: React.FC = () => {
                 <StyledSectionHeading marginbottom={'0'}>
                     <StyledLabel fontsize={'16px'} align={'start'} marginbottom={'0.5rem'}>Customise your home page for a more personalised experience.</StyledLabel>
                 </StyledSectionHeading>
+
+                {widgets.length === 0 &&
+                    <div style={{width: '100%'}}>
+                        <StyledText color={'#ffffff80'} fontsize={'25px'} margintop={'15rem'}>
+                            You have no widgets on your home screen yet.
+                            Add some widgets by clicking on 'Edit Home Page'!
+                        </StyledText>
+                        <StyledText color={'#ffffff80'} fontsize={'18px'} margintop={'0.5rem'}>
+                            You may resize and drag them around to rearrange them!
+                        </StyledText>
+                    </div>
+                }
 
                 <Dashboard dashboardWidth={width} widgets={widgets}/>
                 <WidgetsDrawer dashboardWidth={width} onClose={closeDrawer} open={drawerOpen} setLayout={setWidgets} layout={widgets}/>
